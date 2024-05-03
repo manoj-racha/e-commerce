7cd8ff333df76a95d1b52e662cf43108af727443
manoj-racha <manojracha1947@gmail.com>
2024-05-03 08:20:30 +0530

    added a new section called popular in wonen section to the application and added items in popular in women scetion using item component

---
+++ frontend/src/Components/Item/Item.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Item.css'
+const Item = (props) => {
+  return (
+    <div>
+        <div className="card d-flex m-5 item" style={{ width: '18rem' }}>
+            <img src={props.image} alt="" className='item'/>
+            <div className="card-body">
+                <p className="card-title item-name  " >{props.name}</p>
+                <div className="item-prices d-flex ">
+                    <h6 className="item-price-new card-text me-2 font-weight-bold">
+                        ${props.new_price}
+                    </h6>
+                    <h6 className="item-price-old card-text text-decoration-line-through">
+                        {props.old_price}
+                    </h6>
+                </div>
+            </div>
+</div>
+    </div>
+  )
+}
+
+export default Item
